4aa81d3778ffa22041a6a946886b5352abc33716
blubukshaa <allaboutshaa17@gmail.com>
2025-11-07 07:52:32 +0700

    menambahkan file latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,34 @@
+'''Shafa Stevia Putri 2505976'''
+
+# membuat deret fibonacci dengan menggunakan fungsi
+def fibonacci(x):
+    a, b = 1, 2
+    hasil = []
+    for i in range(x):
+        hasil.append(a)
+        a, b = b, a + b
+    return hasil
+    
+print(fibonacci(5))
+
+# menghitung volume tabung yang dibuat dengan fungsi
+# volume tabung = pi*r*r*t
+def volumeTabung(a, b, c):
+    print(f"Hasilnya adalah {a * b * b * c}")
+
+volumeTabung(22/7, 6, 4)
+
+# Menghitung fungsi nilai total dan nilai rata-ratanya berdasarkan nilai yang diinputkan dari nilai total dimana banyaknya angka tidak dideklarasikan. 
+def total_rata():
+    data = input("Masukkan angka yang akan diinput (masukkan tanpa koma dan pisahkan dengan spasi): ")
+
+    angka = [float(x) for x in data.split()]  # mengubah string menjadi float
+
+    total = sum(angka)
+    rata_rata = total/len(angka)
+
+    print(f"Angka yang telah diinput adalah {angka}")
+    print(f"Total semua angka adalah {total}")
+    print(f"Rata-rata semua angka adalah {rata_rata}")
+
+total_rata()
